d58e492c1773099a55acd6ac541678d83aaee052
matthew <uiwwsw@icloud.com>
2022-02-04 16:07:28 +0900

    v0.1.3
    Signed-off-by: matthew <uiwwsw@icloud.com>

--- __tests__/hb-dialog/default.spec.ts
+++ __tests__/hb-dialog/default.spec.ts
@@ -16,18 +16,32 @@ describe(SHADOW_TAG, () => {
     getShadowElement(SHADOW_TAG)?.remove();
   });
 
-  it(`${SHADOW_TAG}는 화면에 보이지 않아야 한다.`, async () => {
-    expect(getComputedStyle(SHADOW_ELEMENT).display).toEqual("none");
+  it(`${SHADOW_TAG}는 화면에 보이지 않아야 한다.`, (done) => {
+    try {
+      expect(getComputedStyle(SHADOW_ELEMENT).display).toEqual("none");
+      done();
+    } catch (error) {
+      done(error);
+    }
   });
-  it(`${SHADOW_TAG}에 'open'클래스가 있으면 화면에 보여야 한다.`, async () => {
+  it(`${SHADOW_TAG}에 'open'클래스가 있으면 화면에 보여야 한다.`, (done) => {
     SHADOW_ELEMENT.classList.add("open");
-
-    expect(getComputedStyle(SHADOW_ELEMENT).display).not.toEqual("none");
+    try {
+      expect(getComputedStyle(SHADOW_ELEMENT).display).not.toEqual("none");
+      done();
+    } catch (error) {
+      done(error);
+    }
   });
-  it(`${SHADOW_TAG}에 'close-btn' 클릭하면 'open'클래스는 사라진다.`, async () => {
+  it(`${SHADOW_TAG}에 'close-btn' 클릭하면 'open'클래스는 사라진다.`, (done) => {
     SHADOW_ELEMENT.classList.add("open");
     const closeBtn = SHADOW_ROOT?.getElementById("close-btn");
     closeBtn?.click();
-    expect(SHADOW_ELEMENT.className).not.toContain("open");
+    try {
+      expect(SHADOW_ELEMENT.className).not.toContain("open");
+      done();
+    } catch (error) {
+      done(error);
+    }
   }, 2000);
 });
--- __tests__/hb-select/default.spec.ts
+++ __tests__/hb-select/default.spec.ts
@@ -16,18 +16,33 @@ describe(SHADOW_TAG, () => {
     getShadowElement(SHADOW_TAG)?.remove();
   });
 
-  it(`${SHADOW_TAG}의 리스트는 화면에 보이지 않아야 한다.`, async () => {
+  it(`${SHADOW_TAG}의 리스트는 화면에 보이지 않아야 한다.`, (done) => {
     const list = SHADOW_ROOT?.getElementById("list");
-    expect(getComputedStyle(list!).display).toEqual("none");
+    try {
+      expect(getComputedStyle(list!).display).toEqual("none");
+      done();
+    } catch (error) {
+      done(error);
+    }
   });
-  it(`${SHADOW_TAG}에 포커싱되면 'open'클래스가 추가된다.`, async () => {
+  it(`${SHADOW_TAG}에 포커싱되면 'open'클래스가 추가된다.`, (done) => {
     SHADOW_ELEMENT.focus();
-    expect(SHADOW_ELEMENT.className).toContain("open");
+    try {
+      expect(SHADOW_ELEMENT.className).toContain("open");
+      done();
+    } catch (error) {
+      done(error);
+    }
   });
-  it(`${SHADOW_TAG}에 'open'클래스가 있으면 리스트는 화면에 보여야 한다.`, async () => {
+  it(`${SHADOW_TAG}에 'open'클래스가 있으면 리스트는 화면에 보여야 한다.`, (done) => {
     SHADOW_ELEMENT.focus();
     const list = SHADOW_ROOT?.getElementById("list");
-    expect(getComputedStyle(list!).display).not.toEqual("none");
+    try {
+      expect(getComputedStyle(list!).display).not.toEqual("none");
+      done();
+    } catch (error) {
+      done(error);
+    }
   });
   it(`${SHADOW_TAG}에 리스트의 너비는 부모엘리먼트의 너비와 동일해야 한다.`, async () => {
     SHADOW_ELEMENT.focus();
@@ -37,9 +52,14 @@ describe(SHADOW_TAG, () => {
       getComputedStyle(SHADOW_ELEMENT).width
     );
   });
-  it(`${SHADOW_TAG}에 블러되면 'open'클래스가 제거된다.`, async () => {
+  it(`${SHADOW_TAG}에 블러되면 'open'클래스가 제거된다.`, (done) => {
     SHADOW_ELEMENT.classList.add("open");
     SHADOW_ELEMENT.onHide();
-    expect(SHADOW_ELEMENT.className).not.toContain("open");
+    try {
+      expect(SHADOW_ELEMENT.className).not.toContain("open");
+      done();
+    } catch (error) {
+      done(error);
+    }
   }, 0);
 });
