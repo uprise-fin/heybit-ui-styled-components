78288a87438eeba2639fc894202dc7194151a525
matthew <uiwwsw@icloud.com>
2022-02-07 18:07:12 +0900

    sass 관련 설정중
    Signed-off-by: matthew <uiwwsw@icloud.com>

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,34 @@
 module.exports = {
-  webpackFinal: async (config) => {
-    // find web-components rule for extra transpilation
-    const webComponentsRule = config.module.rules.find(
-      (rule) => rule.use && rule.use.options && rule.use.options.babelrc === false
-    );
-    // add your own `my-library`
-    webComponentsRule.test.push(new RegExp(`node_modules(\\/|\\\\)my-library(.*)\\.js$`));
+  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials","@storybook/preset-scss"],
+  "framework": "@storybook/web-components",
+  core: {
+    builder: "webpack5"
+  },
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
 
+    // Make whatever fine-grained changes you need
+    config.module.rules.push(
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "lit-scss-loader",
+            options: {
+              minify: true, // defaults to false
+            },
+          },
+          "extract-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+    );
+
+    // Return the altered config
     return config;
   },
 };
---
+++ src/components/hb-dialog/index.stories.ts
@@ -0,0 +1,25 @@
+import { Meta, Story } from '@storybook/web-components';
+import { html } from 'lit';
+import './index';
+import type { HbDialog } from './index';
+
+// More on default export: https://storybook.js.org/docs/web-components/writing-stories/introduction#default-export
+export default {
+  title: "dialog/Button",
+  component: "hb-dialog",
+} as Meta;
+
+// More on component templates: https://storybook.js.org/docs/web-components/writing-stories/introduction#using-args
+const Template: Story<HbDialog> = ({ value }) =>
+  html`
+    <hb-dialog class="open">
+      <div slot="header">ddd</div>
+      <button slot="footer">ddd</button>
+      <button slot="footer">222</button>
+    </hb-dialog>
+  `;
+
+export const Primary: Story<HbDialog> = Template.bind({});
+Primary.args = {
+  value: 'ddd',
+};
