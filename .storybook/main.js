1185fe41c0cb6cc2ac01aadb3ab14438eee89025
matthew <uiwwsw@icloud.com>
2022-02-07 16:59:41 +0900

    s
    Signed-off-by: matthew <uiwwsw@icloud.com>

---
+++ .storybook/main.js
@@ -0,0 +1,12 @@
+module.exports = {
+  webpackFinal: async (config) => {
+    // find web-components rule for extra transpilation
+    const webComponentsRule = config.module.rules.find(
+      (rule) => rule.use && rule.use.options && rule.use.options.babelrc === false
+    );
+    // add your own `my-library`
+    webComponentsRule.test.push(new RegExp(`node_modules(\\/|\\\\)my-library(.*)\\.js$`));
+
+    return config;
+  },
+};
