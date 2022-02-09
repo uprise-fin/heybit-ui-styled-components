ad7d60a54a339664d0e47823b8f8f8e90a944aff
matthew <uiwwsw@icloud.com>
2022-02-09 14:26:21 +0900

    test
    Signed-off-by: matthew <uiwwsw@icloud.com>

---
+++ src/components/hb-input/index.stories.ts
@@ -0,0 +1,21 @@
+import { Meta, Story } from '@storybook/web-components';
+import { html } from 'lit';
+import "./index";
+import type { HbInput } from './index';
+
+export default {
+  title: "hb-input",
+  component: "hb-input",
+} as Meta;
+
+// More on component templates: https://storybook.js.org/docs/web-components/writing-stories/introduction#using-args
+const Template: Story<HbInput> = ({ value }) =>
+  html`
+    <hb-input value=${value}>
+    </hb-input>
+  `;
+
+export const primary: Story<HbInput> = Template.bind({});
+primary.args = {value: '빈값'}
+export const primary2: Story<HbInput> = Template.bind({});
+primary.args = {value: '빈값'}
