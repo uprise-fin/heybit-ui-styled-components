f89a41d37b2a7f5bf4a4c478a321353880a0b958
matthew <uiwwsw@icloud.com>
2022-05-16 10:28:59 +0900

    chore: v0.8.1
    Signed-off-by: matthew <uiwwsw@icloud.com>

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
 import { HbButton } from "./components/forms/hb-button";
 import { HbInput } from "./components/forms/hb-input";
 import { HbSelect } from "./components/forms/hb-select";
+import { HbIcon } from "./components/hb-icon";
+import { HbImg } from "./components/hb-img";
 import { HbDialog } from "./components/layers/hb-dialog";
 import { HbSpinner } from "./components/layers/hb-spinner";
 import { HbSkeleton } from "./components/layers/hb-skeleton";
-import { HbIcon } from "./components/hb-icon";
-import { HbImg } from "./components/hb-img";
 export {
-  HbSelect,
-  HbDialog,
-  HbSpinner,
-  HbSkeleton,
-  HbInput,
   HbButton,
+  HbInput,
+  HbSelect,
   HbIcon,
   HbImg,
+  HbDialog,
+  HbSkeleton,
+  HbSpinner,
 };
