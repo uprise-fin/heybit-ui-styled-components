e7a8a31942cb118cc158bd9d2e32c08948e9b134
matthew <uiwwsw@icloud.com>
2022-06-28 09:01:07 +0900

    feat: add husky
    Signed-off-by: matthew <uiwwsw@icloud.com>

---
+++ commitlint.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "build",
+        "ci",
+        "chore",
+        "revert",
+      ],
+    ],
+    // 한글 커밋의 경우, 어순 때문에 대문자가 앞에 나올 수 있음.
+    // 영문의 경우- build: add Dockerfile
+    // 한글의 경우 - build: Dockerfile 추가
+    // 아래를 허용하기 위해 설정 덮어씀.
+    "subject-case": [2, "never", []],
+  },
+};
