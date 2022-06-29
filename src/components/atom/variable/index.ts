23144d57349acb4b05a49a65ecf6e7e3645734c6
matthew <uiwwsw@icloud.com>
2022-06-29 17:33:55 +0900

    feat: remove scss variables
    Signed-off-by: matthew <uiwwsw@icloud.com>

--- src/components/atom/variable/index.ts
+++ src/components/atom/variable/index.ts
@@ -21,20 +21,70 @@ export const colorPalette = Object.entries(colors).reduce(
 ) as Obj<Matercolor, theme>;
 export const variables = {
   font: {
+    family: `Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto,
+  "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR",
+  "Malgun Gothic", sans-serif`,
     color: {
       void: white,
       solid: colorPalette[theme.black][900],
     },
+    size: 16,
   },
   border: {
+    width: 1,
+    color: colorPalette[theme.black][200],
     radius: 4,
+  },
+  background: {
+    color: white,
+  },
+  transition: {
+    duration: "300ms",
   },
 };
 export const components = {
+  dim: {
+    background: {
+      color: colorPalette[theme.black][900],
+    },
+  },
+  list: {
+    height: 50,
+    padding: {
+      right: 10,
+      left: 10,
+    },
+    font: {
+      size: variables.font.size,
+    },
+    border: {
+      width: variables.border.width,
+      color: variables.border.color,
+    },
+  },
+  select: {
+    font: {
+      size: variables.font.size,
+    },
+  },
+  input: {
+    height: 50,
+    border: {
+      width: variables.border.width,
+      color: variables.border.color,
+      "color--error": colorPalette.yellow[300],
+    },
+    padding: {
+      right: 10,
+      left: 10,
+    },
+    font: {
+      size: variables.font.size,
+    },
+  },
   modal: {
     width: 380,
     margin: 20,
-    dim: colorPalette[theme.black][900],
     background: white,
     dialog: {
       "padding-top": 52,
