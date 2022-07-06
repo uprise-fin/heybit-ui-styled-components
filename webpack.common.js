7317d84874d4a0d3d9a407ac4e1cef89432c1d63
matthew <uiwwsw@icloud.com>
2022-07-06 15:59:04 +0900

    chore: edit eslint
    Signed-off-by: matthew <uiwwsw@icloud.com>

--- webpack.common.js
+++ webpack.common.js
@@ -1,80 +1,81 @@
-const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const Dotenv = require("dotenv-webpack");
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 module.exports = {
   resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx"],
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
     alias: {
-      "@src": "./src", // 사용할 경로
+      '@': path.resolve(__dirname, 'src'),
+      '~': path.resolve(__dirname, '/'),
     },
   },
   entry: {
-    initial: [path.resolve(__dirname, "./src/initial.scss")],
-    onlyStyled: [path.resolve(__dirname, "./src/styles/index.scss")],
-    index: [path.resolve(__dirname, "./src/index.ts")],
+    initial: [path.resolve(__dirname, './src/initial.scss')],
+    onlyStyled: [path.resolve(__dirname, './src/styles/index.scss')],
+    index: [path.resolve(__dirname, './src/index.ts')],
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: "ts-loader",
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
       {
         test: /\.scss$/,
         exclude: [
-          path.resolve(__dirname, "./src/initial.scss"),
-          path.resolve(__dirname, "./src/styles/index.scss"),
+          path.resolve(__dirname, './src/initial.scss'),
+          path.resolve(__dirname, './src/styles/index.scss'),
         ],
         use: [
           {
-            loader: "lit-scss-loader",
+            loader: 'lit-scss-loader',
             options: {
               minify: true, // defaults to false
             },
           },
-          "extract-loader",
-          "css-loader",
-          "sass-loader",
+          'extract-loader',
+          'css-loader',
+          'sass-loader',
         ],
       },
       {
         test: /\.scss$/,
         include: [
-          path.resolve(__dirname, "./src/initial.scss"),
-          path.resolve(__dirname, "./src/styles/index.scss"),
+          path.resolve(__dirname, './src/initial.scss'),
+          path.resolve(__dirname, './src/styles/index.scss'),
         ],
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
       {
         test: /\.svg$/,
-        loader: "svg-inline-loader",
+        loader: 'svg-inline-loader',
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset",
+        type: 'asset',
       },
     ],
   },
   plugins: [new MiniCssExtractPlugin(), new CleanWebpackPlugin(), new Dotenv()],
 
   resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx", ".svg", ".css", ".scss"],
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.svg', '.css', '.scss'],
   },
   optimization: {
     minimize: true,
-    minimizer: [new TerserPlugin({ extractComments: false })],
+    minimizer: [new TerserPlugin({extractComments: false})],
   },
   // plugins: [new FixStyleOnlyEntriesPlugin(), new MiniCssExtractPlugin()],
 
   output: {
-    path: path.resolve(__dirname, "./dist"),
-    filename: "[name].js",
-    library: "heybit-ui-styled-components",
-    libraryTarget: "umd",
-    globalObject: "this",
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].js',
+    library: 'heybit-ui-styled-components',
+    libraryTarget: 'umd',
+    globalObject: 'this',
     umdNamedDefine: true,
   },
 };
