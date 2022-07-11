7fb8723c21afac19fefa21efc4a29a917490db55
matthew <uiwwsw@icloud.com>
2022-07-11 09:42:25 +0900

    chore: edit storybook

--- src/components/molecule/toast/index.stories.ts
+++ src/components/molecule/toast/index.stories.ts
@@ -1,6 +1,6 @@
 import {Meta, Story} from '@storybook/web-components';
 import {html} from 'lit';
-import {hbButtonTheme} from '../../organism/button';
+import {hbButtonTheme, hbButtonType} from '../../organism/button';
 import './index';
 import type {HbToast} from './index';
 
@@ -37,9 +37,13 @@ const Template: Story<HbToastExns> = ({visibleIcon, contents, icons}) => {
     };
     element.messages = [...element.messages, message];
   }
-  return html`<hb-button theme=${hbButtonTheme.primary} @event=${reset}
+  return html`<hb-button
+      type=${hbButtonType.rectagle}
+      theme=${hbButtonTheme.primary}
+      @event=${reset}
       >토스트리셋</hb-button
-    ><br /><hb-button @event=${event}>토스트 추가하기</hb-button
+    ><br /><hb-button type=${hbButtonType.rectagle} @event=${event}
+      >토스트 추가하기</hb-button
     ><hb-toast id="toast"></hb-toast>`;
 };
 export const primary: Story<HbToastExns> = Template.bind({});
