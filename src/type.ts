c20aa9c59ccc5f0a4c397ce713633ba389231f37
matthew <uiwwsw@icloud.com>
2022-07-11 11:26:19 +0900

    test: type.ts
    Signed-off-by: matthew <uiwwsw@icloud.com>

---
+++ src/type.ts
@@ -0,0 +1,3 @@
+import {hbButtonTheme, hbButtonType} from './components/organism/button';
+import {HbDialogAnchor, HbDialogButton} from './components/template/dialog';
+export {hbButtonTheme, hbButtonType, HbDialogAnchor, HbDialogButton};
