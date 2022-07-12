f2de74c757264081c4a5fb19d172a030df4190ff
matthew <uiwwsw@icloud.com>
2022-07-12 13:20:29 +0900

    test: remove type.ts

--- src/components/type.ts
+++
@@ -1,7 +0,0 @@
-import * as HbVariable from './atom/variable/type';
-import * as HbTransition from './atom/transition/type';
-import * as HbToast from './molecule/toast/type';
-import * as HbButton from './organism/button/type';
-import * as HbModal from './template/modal/type';
-
-export {HbVariable, HbTransition, HbToast, HbButton, HbModal};
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-import {
-  HbVariable,
-  HbTransition,
-  HbToast,
-  HbButton,
-  HbModal,
-} from '@/components/type';
+import * as HbVariable from '@/components/atom/variable/type';
+import * as HbTransition from '@/components/atom/transition/type';
+import * as HbToast from '@/components/molecule/toast/type';
+import * as HbButton from '@/components/organism/button/type';
+import * as HbModal from '@/components/template/modal/type';
+
 export {HbVariable, HbTransition, HbToast, HbButton, HbModal};
