23d386a9045811e2a649698cd82087dc36c909a6
matthew <uiwwsw@icloud.com>
2022-08-04 17:40:11 +0900

    feat: tooltip

--- src/components/molecule/tooltip/index.stories.ts
+++ src/components/molecule/tooltip/index.stories.ts
@@ -1,28 +1,27 @@
+import {Size} from '@/components/atom/variable/type';
 import {Meta, Story} from '@storybook/web-components';
 import {html} from 'lit';
-import {Size} from '@/components/atom/variable/type';
-import {dev} from '@/utils';
+import {HbIconName} from '../icon/type';
 import './index';
 import type {HbTooltip} from './index';
 
-export default dev() &&
-  ({
-    title: 'components/molecule/hb-tooltip',
-    component: 'hb-tooltip',
-    argTypes: {
-      open: {
-        options: [false, true],
-        control: {type: 'radio'},
-      },
+export default {
+  title: 'components/molecule/hb-tooltip',
+  component: 'hb-tooltip',
+  argTypes: {
+    open: {
+      options: [false, true],
+      control: {type: 'radio'},
     },
-  } as Meta);
+  },
+} as Meta;
 
 // More on component templates: https://storybook.js.org/docs/web-components/writing-stories/introduction#using-args
 const Template: Story<HbTooltip> = ({open}) =>
   html`안녕하세요 헤이비트는
     <hb-tooltip ?open=${open}
       ><hb-icon
-        icon="ic-my-setting-renewal"
+        icon=${HbIconName['system/outline/info']}
         slot="front"
         size=${Size.small}
       ></hb-icon>
