226c4a4965cf752acdb3251d3e3fb25b7dbdf3e7
matthew <uiwwsw@icloud.com>
2022-09-27 16:39:46 +0900

    feat: tabIndex 추가

--- src/components/organism/input/index.ts
+++ src/components/organism/input/index.ts
@@ -247,6 +247,7 @@ export class HbInput extends InitAttribute<HbInputProps> {
       this.shadowRoot,
       'input',
     );
+    this.tabIndex = 0;
     this.inputEl = inputEl;
     this.value = this.getAttribute('value');
     inputEl.value = this.value;
