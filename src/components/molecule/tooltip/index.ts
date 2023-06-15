58036ebff05b694cc1a35d2faa702a19856e9e9c
hckang80 <hckang80@gmail.com>
2023-06-15 18:19:09 +0900

    refactor: 불필요한 코드 제거

--- src/components/molecule/tooltip/index.ts
+++ src/components/molecule/tooltip/index.ts
@@ -1,18 +1,12 @@
 import '@/components/atom/icon';
-import '@/components/atom/transition';
 import { Base } from '@/components/base';
 import { html } from 'lit';
 import { customElement } from 'lit/decorators.js';
 
 /**
- * @property open 온 오프
- * @slot header - optional, 헤더
- * @slot content - optional, 내용
- * @slot footer - optional, 푸터
- * @csspart container
- * @csspart header
+ * @slot front, 오프너
+ * @csspart front
  * @csspart content
- * @csspart footer
  */
 
 @customElement('hb-tooltip')
@@ -22,8 +16,6 @@ export class HbTooltip extends Base {
   }
 
   open: boolean = false;
-
-  position = [false, false];
 
   static get properties() {
     return {
@@ -37,26 +29,13 @@ export class HbTooltip extends Base {
   async connectedCallback() {
     await super.connectedCallback();
     this.tabindex = '0';
-    this.onfocus = this.onOpen;
-    this.onblur = () => {
-      this.open = false;
-    };
-    this.onmouseenter = () => this.focus();
-    this.onmouseleave = () => this.blur();
+    this.onmouseleave = () => this.hasAttribute('open') && this.removeAttribute('open');
+    this.onblur = () => this.hasAttribute('open') && this.removeAttribute('open');
   }
 
   disconnectedCallback() {
-    this.onfocus = () => null;
-    this.onblur = () => null;
-    this.onmouseenter = () => null;
     this.onmouseleave = () => null;
-  }
-
-  onOpen() {
-    const { x, y, width, height } = this.getBoundingClientRect();
-    const { innerWidth, innerHeight } = window;
-    this.position = [x + width / 2 > innerWidth / 2, y + height / 2 > innerHeight / 2];
-    this.open = true;
+    this.onblur = () => null;
   }
 
   render() {
@@ -72,42 +51,3 @@ declare global {
     'hb-tooltip': HbTooltip;
   }
 }
-// const NAME = "hb-tooltip";
-// customElements.get(NAME) ||
-//   customElements.define(
-//     NAME,
-//     class extends CustomElement {
-//       template = require(`./${NAME}.hbs`);
-//       css = require(`./${NAME}.scss`).default;
-//       properties = {
-//         id: {
-//           closeBtn: "close-btn",
-//           wrap: "wrap",
-//         },
-//       };
-//       constructor() {
-//         super();
-//       }
-//       connectedCallback(): void {
-//         super.connectedCallback();
-//         this.isWrapEl.onanimationstart = () => this.onAnimationStart();
-//         this.isWrapEl.onanimationend = () => this.onAnimationEnd();
-//         this.isCloseBtnEl.onclick = () => this.onHide();
-//       }
-//       get isCloseBtnEl() {
-//         return this.shadowRoot.getElementById(
-//           this.isProperties.id.closeBtn
-//         ) as HTMLButtonElement;
-//       }
-//       get isWrapEl() {
-//         return this.shadowRoot.getElementById(
-//           this.isProperties.id.wrap
-//         ) as HTMLButtonElement;
-//       }
-
-//       onHide() {
-//         this.classList.add(this.isProperties.classList.animation);
-//         super.onHide();
-//       }
-//     }
-//   );
