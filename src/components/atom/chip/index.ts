f193b60d978b8f18a472c8909f45fe7982bb8562
hckang80 <hckang80@gmail.com>
2023-08-07 14:21:22 +0900

    feat: 데코레이터 방식으로 개선

--- src/components/atom/chip/index.ts
+++ src/components/atom/chip/index.ts
@@ -1,18 +1,9 @@
 import { html } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import { customElement, property } from 'lit/decorators.js';
 import { Base } from '@/components/base';
 /**
- * @fires change 값이 변경될때 발생
- * @property attributeSync true 시 value값이 arrtibute 싱크됨
- * @property fixed true 시 overflow에서 넘어감
- * @property value 기본 값
- * @property options Options[] 옵션
- * @property search 서치 온오프
- * @slot icon - optional, icon부분을 커스텀할때 사용
- * @slot caret - optional, caret부분을 커스텀할때 사용
- * @slot option - required, select의 옵션 엘리먼트
- * @csspart label
- * @csspart list
+ * @property color 지정 테마
+ * @slot - 기본 slot
  */
 
 @customElement('hb-chip')
@@ -21,23 +12,8 @@ export class HbChip extends Base {
     return [require('./style.scss').default];
   }
 
-  _color: string = '';
-
-  get color() {
-    return this._color;
-  }
-
-  set color(value: string) {
-    this._color = value;
-    if (value) this.setAttribute('color', value);
-    else this.removeAttribute('color');
-  }
-
-  static get properties() {
-    return {
-      color: { type: String, Reflect: true }
-    };
-  }
+  @property({ reflect: true })
+  color = '';
 
   render() {
     return html` <slot></slot> `;
