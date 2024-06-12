c61c8f35276234f38df09442eeff1b1e6a420d18
hckang80 <hckang80@users.noreply.github.com>
2024-06-12 06:33:35 +0000

    deploy: eb50fd281e052ace74b4e7d8e4674bd2951069a2

---
+++ how-to-use-css-stories.7a7c7941.iframe.bundle.js
@@ -0,0 +1,16 @@
+"use strict";(self.webpackChunkheybit_ui_styled_components=self.webpackChunkheybit_ui_styled_components||[]).push([[3644],{"./src/how-to-use/css.stories.ts":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,{__namedExportsOrder:()=>__namedExportsOrder,default:()=>__WEBPACK_DEFAULT_EXPORT__,hbButton:()=>hbButton});var lit__WEBPACK_IMPORTED_MODULE_0__=__webpack_require__("./node_modules/.pnpm/lit@2.8.0/node_modules/lit/index.js");const __WEBPACK_DEFAULT_EXPORT__={title:"how-to-use/css"},hbButton={render:()=>lit__WEBPACK_IMPORTED_MODULE_0__.dy`
+    <div class="hb-button" theme="primary" size="large" type="rectangle" tabindex="0">
+      <!---->
+      <!-- <div
+        name="slot--left"
+        part="slot--left"
+        class="hb-button__slot hb-button__slot--left"
+      ></div> -->
+      <div class="hb-button__label">primary large block</div>
+      <!-- <div
+        name="slot--right"
+        part="slot--right"
+        class="hb-button__slot hb-button__slot--right"
+      ></div> -->
+    </div>
+  `};hbButton.parameters={...hbButton.parameters,docs:{...hbButton.parameters?.docs,source:{originalSource:"{\n  render: () => Template()\n}",...hbButton.parameters?.docs?.source}}};const __namedExportsOrder=["hbButton"]}}]);
