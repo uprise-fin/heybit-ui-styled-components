c331db3445470ac2769ecbde85b904984449f1ca
medmemara <medmemara@users.noreply.github.com>
2024-11-25 14:11:19 +0000

    deploy: 2eae6bb98167c6a2b7411b3a629f3cdf7e76f59f

---
+++ components-container-paper-stories.1d469ed9.iframe.bundle.js
@@ -0,0 +1,6 @@
+"use strict";(self.webpackChunkheybit_ui_styled_components=self.webpackChunkheybit_ui_styled_components||[]).push([[299],{"./src/components/container/paper.stories.ts":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,{NoneStory:()=>NoneStory,__namedExportsOrder:()=>__namedExportsOrder,default:()=>paper_stories});var lit=__webpack_require__("./node_modules/.pnpm/lit@2.8.0/node_modules/lit/index.js");lit.iv`.paper{display:flex;flex-direction:column;justify-content:center;align-items:center;text-align:center}`;const paper_stories={title:"Container/Paper",tags:["autodocs"],render:args=>(({})=>lit.dy`
+    <div class="paper" style="height:200px">
+      <h2>제목입니다.</h2>
+      <div>paper는 height의 센터에 내용이 설정됩니다.</div>
+    </div>
+  `)(args),controls:{hideNoControlsWarning:!0},argTypes:{}},NoneStory={args:{}};NoneStory.parameters={...NoneStory.parameters,docs:{...NoneStory.parameters?.docs,source:{originalSource:"{\n  args: {}\n}",...NoneStory.parameters?.docs?.source}}};const __namedExportsOrder=["NoneStory"]}}]);
